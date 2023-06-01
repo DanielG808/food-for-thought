8111a4405645a9b46edebcc217187202725e8a08
Chris <christopherlee2477@gmail.com>
2023-06-01 12:49:02 -0400

    got mutiple pins to show up

--- assets/googleapi.js
+++ assets/googleapi.js
@@ -16,11 +16,18 @@ function initMap() {
     fields: ["name", "geometry"],
   }
 
+  const trying = {
+    query: "ingles",
+    fields: ["name", "geometry"],
+  }
+
   service = new google.maps.places.PlacesService(map);
   console.log(google.maps.places.PlacesService(map))
-  service.findPlaceFromQuery(request, (results, status) => {
-    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+  service.findPlaceFromQuery(trying, (results, status) => {
       console.log(results)
+      console.log(status)
+      console.log(google.maps.places.PlacesServiceStatus)
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
       for (let i = 0; i < results.length; i++) {+
         createMarker(results[i]);
       }
