d9d51c8fe7f5da6ae72e15d463e6598ba0b2d459
Tegaomare <tegaomare@oghenetegasimac.mynetworksettings.com>
2023-06-01 14:02:46 -0400

    edited comments

--- assets/googleapi.js
+++ assets/googleapi.js
@@ -7,7 +7,10 @@ let userLocation;
 
 function centerMapOnUserLocation(position) {
   //getting user location
-  userLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  userLocation = new google.maps.LatLng(
+    position.coords.latitude,
+    position.coords.longitude
+  );
 
   infowindow = new google.maps.InfoWindow();
   map = new google.maps.Map(document.getElementById("map"), {
@@ -16,26 +19,25 @@ function centerMapOnUserLocation(position) {
   });
   //putting the markers on the map
   drawLocaationMarkers();
-
 }
 
-function drawLocaationMarkers(){
+function drawLocaationMarkers() {
   const request = {
     //finding stores in a 5kilometer radius
     location: userLocation,
-    radius: '5000',
+    radius: "5000",
     type: ["store"],
-  }
+  };
 
   service = new google.maps.places.PlacesService(map);
-  console.log(service)
+  console.log(service);
   service.nearbySearch(request, (results, status) => {
-      console.log(results)
-      console.log(status)
-      console.log(google.maps.places.PlacesServiceStatus)
+    console.log(results);
+    console.log(status);
+    console.log(google.maps.places.PlacesServiceStatus);
     if (status === google.maps.places.PlacesServiceStatus.OK && results) {
-      for (let i = 0; i < results.length; i++) {+
-        createMarker(results[i]);
+      for (let i = 0; i < results.length; i++) {
+        +createMarker(results[i]);
       }
 
       map.setCenter(results[0].geometry.location);
@@ -46,16 +48,14 @@ function drawLocaationMarkers(){
 function initMap() {
   // get user location
 
-  navigator.geolocation.getCurrentPosition(centerMapOnUserLocation)
-
-
+  navigator.geolocation.getCurrentPosition(centerMapOnUserLocation);
 }
 
 function createMarker(place) {
   if (!place.geometry || !place.geometry.location) return;
 
   const icon = {
-    url: "./assets/images/grocery.png", // Provide the path to your custom icon image
+    url: "./assets/images/grocery.png", // Provide the path to our custom icon image
     scaledSize: new google.maps.Size(50, 50), // Adjust the size of the icon as needed
   };
 
